6b53dfbe9af7286059fbbc8ea3036abde5f8d249
amineNouabi <aminenouabi9@gmail.com>
2024-10-11 15:42:14 +0100

    Annotated function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+
+Module for adding two floats.
+
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floats and return the result as a float."""
+    return a + b
