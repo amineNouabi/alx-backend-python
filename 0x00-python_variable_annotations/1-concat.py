d074287874182c9219d68dabebd747fc2c8583ab
amineNouabi <aminenouabi9@gmail.com>
2024-10-11 15:45:08 +0100

    task 1 and 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+
+Module for concatenating two strings.
+
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """Concatenate two strings and return the result as a string."""
+    return a + b
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+
+Module for floor of a float.
+
+"""
+
+from math import floor
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float as an int."""
+    return floor(n)
