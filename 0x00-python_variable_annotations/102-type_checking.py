b26d08d0e8dbfd179d75d99768723bfe6a4d336a
amineNouabi <aminenouabi9@gmail.com>
2024-10-11 16:58:14 +0100

    fixed docs

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -10,6 +10,7 @@ from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Return a list with the same elements repeated by factor times."""
     zoomed_in: List[int] = [
         item for item in lst
         for i in range(factor)
