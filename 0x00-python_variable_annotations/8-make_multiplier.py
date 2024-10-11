0d9fc2a7603efc3eb081d3cb5b439397452c8c70
amineNouabi <aminenouabi9@gmail.com>
2024-10-11 16:19:49 +0100

    task 8 and 9

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -8,7 +8,8 @@ Module for multiplying two floats.
 
 
 def make_multiplier(multiplier: float) -> callable:
-    """Return a function that multiplies a float by multiplier."""
+    """Return a function that multiplies
+       a float by multiplier."""
     def multiplier_fn(n: float) -> float:
         """Return the product of n and multiplier."""
         return n * multiplier
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -9,6 +9,8 @@ Module for element length.
 from typing import Sequence, Union, Any, List
 
 
-def element_length(lst: Sequence[Union[Sequence[Any], Any]]) -> List[int]:
+def element_length(
+        lst: Sequence[Union[Sequence[Any], Any]]
+) -> List[int]:
     """Return the length of elements in a sequence."""
     return [len(i) for i in lst]
