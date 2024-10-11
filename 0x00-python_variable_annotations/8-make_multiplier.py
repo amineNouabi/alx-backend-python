b150f8fdf9623bba5a481e67522c35d96d04bd16
amineNouabi <aminenouabi9@gmail.com>
2024-10-11 16:41:20 +0100

    cleaned

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,8 +6,10 @@ Module for multiplying two floats.
 
 """
 
+from typing import Callable
 
-def make_multiplier(multiplier: float) -> callable:
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Return a function that multiplies
        a float by multiplier."""
     def multiplier_fn(n: float) -> float:
